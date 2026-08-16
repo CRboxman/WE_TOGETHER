be8b1e80f79986d9715bffadb08151495c3df679
xiaowugui123836 <625396199@qq.com>
2026-08-16 18:47:02 +0800

    改了1个脚本
    汤哥牛逼

--- Assets/XiaoWuGui/script_test.cs
+++ Assets/XiaoWuGui/script_test.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class script_test : MonoBehaviour
 {
     // Start is called before the first frame update
+    public int tmp = 1;
+    
     void Start()
     {
+        print(tmp);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print(tmp);
     }
 }
