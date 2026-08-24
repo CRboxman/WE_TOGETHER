19b5e17b39e85a7dce3e9a6cde3bd233f8a28765
tang tang <1524533474@qq.com>
2026-08-24 12:26:03 +0800

    相机跟随，重力和碰撞体

---
+++ Assets/New Folder 1/CameraFollow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothTime = 0.2f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void LateUpdate()
+    {
+        if (target == null)
+            return;
+
+        Vector3 targetPosition = new Vector3(
+            target.position.x,
+            target.position.y,
+            transform.position.z
+        );
+
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            targetPosition,
+            ref velocity,
+            smoothTime
+        );
+    }
+}
