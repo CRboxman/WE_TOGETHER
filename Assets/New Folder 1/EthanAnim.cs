dd1dd36f2005d9faca969cbf8ade8bb175c1da43
tang tang <1524533474@qq.com>
2026-08-24 11:43:06 +0800

    ad键移动空格跳跃

---
+++ Assets/New Folder 1/EthanAnim.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EthanAnim : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetInteger("Speed",(int)Input.GetAxisRaw("Horizontal"));
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("DoJump",true);
+        }
+    }
+
+    private void JumpOver()
+    {
+        anim.SetBool("DoJump",false);
+    }
+}
